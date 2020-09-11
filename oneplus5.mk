6d67745959ab671d17f6560daf73ff58c0ab9655
Jiyong Park <jiyong@google.com>
2020-09-11 23:59:20 +0530

    oneplus5(t):  Move VNDK APEX to vendor partition
    ... for aosp_* targets so that we don't need to update vendor.img everytime
    there is a change in the VNDK libraries
    
    Bug: 140136207
    Test: m
    Change-Id: Ic7ffaa9503902884f219400cf93516a6fbf8b210
    (cherry picked from commit 1d97f7055550d375aa7f371e148c3ec9aff0bd29)

--- oneplus5.mk
+++ oneplus5.mk
@@ -227,8 +227,9 @@ PRODUCT_TARGET_VNDK_VERSION := 29
 PRODUCT_EXTRA_VNDK_VERSIONS := 29
 
 PRODUCT_PACKAGES += \
-     libprotobuf-cpp-lite \
-     vndk-sp
+    libprotobuf-cpp-lite \
+    vndk-sp \
+    com.android.vndk.current.on_vendor
     
 PRODUCT_COPY_FILES += \
     prebuilts/vndk/v29/arm64/arch-arm64-armv8-a/shared/vndk-core/libprotobuf-cpp-full.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libprotobuf-cpp-full-v29.so \
