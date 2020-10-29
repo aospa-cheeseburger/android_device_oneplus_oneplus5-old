10d2694bd74d4ac94066a8fdb69824efe8e542e3
luk1337 <priv.luk@gmail.com>
2020-10-29 14:04:13 +0000

    oneplus5(t): Switch to Thermal HAL 2.0
    Co-authored-by: shivatejapeddi <shivatejapeddi@gmail.com>

--- oneplus5.mk
+++ oneplus5.mk
@@ -259,9 +259,7 @@ PRODUCT_PACKAGES += \
 
 # Thermal
 PRODUCT_PACKAGES += \
-    android.hardware.thermal@1.0-impl \
-    android.hardware.thermal@1.0-service \
-    thermal.msm8998
+     android.hardware.thermal@2.0-service.qti
 
 # USB
 PRODUCT_PACKAGES += \
